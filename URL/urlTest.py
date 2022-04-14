7139ac7ed93efcec268158dae4b0e33521f21c29
juniorMSG <milk6645@gmail.com>
2022-04-14 18:56:57 +0900

    url test

---
+++ URL/urlTest.py
@@ -0,0 +1,7 @@
+from urllib import parse
+
+if __name__ == '__main__':
+
+    url = 'https://en.hd-dojki.com/search?text=우성카지노【카톡dptm1010】⑿우성카지노검증완료Ｇ납득하다│우성카지노검증완료㏊우성카지노빠른환전ラ우성카지노먹튀없음′우성카지노먹튀없음ド우성카지노먹튀없음Ｅ우성카지노⒫abetters'
+    parse.urlparse(url)
+
